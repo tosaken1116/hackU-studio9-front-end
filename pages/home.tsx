f11123a677fa8befd1e26273e35955a66a48b96a
Aopaka1231 <hiroyuki_kuroiwa@icloud.com>
2023-02-16 02:45:25 +0900

    いろいろ追加

---
+++ pages/home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function home() {
+  return (
+    <div>
+      <h1>username</h1>
+      <h1 style={{textAlign: "center"}}>
+        業務一覧
+        <p></p>
+      </h1>
+    </div>
+  )
+}
