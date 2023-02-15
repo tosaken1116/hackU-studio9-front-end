61182ee1f856d0314c64c1c17734bdf71a7853b0
Aopaka1231 <hiroyuki_kuroiwa@icloud.com>
2023-02-16 03:58:40 +0900

    desktopに移動

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { Button } from '@mui/material'
+import React, { useState } from 'react'
+import Modal from 'react-modal'
 
 export default function home() {
   return (
@@ -6,8 +8,9 @@ export default function home() {
       <h1>username</h1>
       <h1 style={{textAlign: "center"}}>
         業務一覧
-        <p></p>
+        <p>業務1</p>
       </h1>
+      <Button href='./directmessage'>DM</Button>
     </div>
   )
 }
